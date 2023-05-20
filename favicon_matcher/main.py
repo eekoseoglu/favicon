056d2b61f83da66852fcbaf913c7ae591bf3d0fa
Emre <eekoseoglu@gmail.com>
2023-05-20 22:09:35 +0200

    first complete working version

--- favicon_matcher/main.py
+++ favicon_matcher/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from md5hash import MD5Hash
 from xml_file_parser import XMLFileParser
+import socket
 
 app = FastAPI()
 
 @app.get("/my-first-api")
 def hello(url: str):
-    print(url)
+    host_name = socket.gethostname()
     md5_hash_generator = MD5Hash(url)
     hash = md5_hash_generator.get_hash()
     my_parser = XMLFileParser("favicons.xml")
@@ -14,6 +15,12 @@ def hello(url: str):
     fingerprints = my_dict["fingerprints"]["fingerprint"]
     for fingerprint in fingerprints:
         if hash in fingerprint["example"]:
-            return {'This is my dict: ' + str(fingerprint["param"])} 
+            return {host_name : 'This is my dict: ' + str(fingerprint["param"])} 
     return None
 
+
+
+# docker build -t ekose/deneme:1.0 . --network=host
+# docker run -p 8000:8000 ekose/hasher:1.1
+# docker compose up -d
+# docker-compose up --build --remove-orphans --scale app=3
